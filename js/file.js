983696d5be042915cd0197c84803e0efb2b5e796
Vittorio Fusco <fuscovittorio89@gmail.com>
2023-10-11 17:13:31 +0200

    first commit

---
+++ js/file.js
@@ -0,0 +1,10 @@
+const main = document.getElementsByTagName("main")[0];
+const lista = document.createElement("div");
+lista.className = "lista";
+main.appendChild(lista);
+console.log(lista);
+
+button.onclick = function (e) {
+  const clickBtn = e.target;
+  clickBtn;
+};
